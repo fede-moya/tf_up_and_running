cba51d48fbc23d258043e079498993eb1252d480
Federico Moyá <federicomoyamartin@gmail.com>
2023-07-18 20:10:22 -0300

    Functional modules

--- chapter4/prod/services/webserver-cluster/main.tf
+++ chapter4/prod/services/webserver-cluster/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     profile = "terraform_up_and_running"
     bucket         = "terraform-up-and-running-fmoya"
-    key            = "stage/services/webserver-cluster/terraform.tfstate"
+    key            = "prod/services/webserver-cluster/terraform.tfstate"
     region         = "us-east-2"
     dynamodb_table = "terraform-up-and-running-locks"
     encrypt        = true
@@ -16,20 +16,28 @@ provider "aws" {
 
 module "webserver_cluster" {
  source = "../../../modules/services/webserver-cluster"
- cluster_name = "webserver-stage"
+ cluster_name = "webservers-prod"
  db_remote_state_bucket = "terraform-up-and-running-fmoya"
- db_remote_state_key = "stage/data-stores/mysql/terraform.tfstate"
+ db_remote_state_key = "prod/data-stores/mysql/terraform.tfstate"
  instance_type = "t2.micro"
  min_size = 2
  max_size = 2
 }
 
-
 resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
   scheduled_action_name = "scale_out_during_business_hours"
   min_size              = 2
   max_size              = 10
-  desired_capacity      = 2
+  desired_capacity      = 10
   recurrence            = "0 9 * * *"
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
+
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  scheduled_action_name = "scale_in_at_night"
+  min_size              = 2
+  max_size              = 10
+  desired_capacity      = 2
+  recurrence            = "0 17 * * *"
   autoscaling_group_name = module.webserver_cluster.asg_name
 }
